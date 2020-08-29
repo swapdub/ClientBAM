65c55e4ff7c37ba5b171faa16c5c3f3d02078d51
SwapDub <32624172+sfd5205@users.noreply.github.com>
2020-08-29 03:46:45 -0400

    idk y i cant push this yet

---
+++ Soojan_version.py
@@ -0,0 +1,103 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import pandas as pd
+
+
+# %%
+
+file_path = "D:\SavAi\ClientBAM\seamless2@clientbam.com-contacts-2020-06-30-08-03-23-cleaned - Copy.csv"
+try:
+  base_df = pd.read_csv(file_path+"cleaned_data.csv") #path of database file
+  main_df = base_df.drop_duplicates()
+except:
+  main_df = pd.DataFrame(columns = ['UpdatedAt', 'Name', 'FirstName', 'MiddleName', 'LastName', 'Title',
+       'Company Name', 'Website', 'List', 'Intel', 'Contact Location',
+       'ContactCity', 'ContactState', 'ContactStateAbbr', 'ContactCountry',
+       'ContactCountryAlpha2', 'ContactCountryAlpha3', 'ContactCountryNumeric',
+       'Company Location', 'CompanyCity', 'CompanyState', 'CompanyStateAbbr',
+       'CompanyCountry', 'CompanyCountryAlpha2', 'CompanyCountryAlpha3',
+       'CompanyCountryNumeric', 'CompanyStaffCountRange',
+       'CompanyRevenueRange', 'Email1', 'EmValidation1', 'Total AI1', 'Email2',
+       'EmValidation2', 'Total AI2', 'ContactPhone1', 'CompanyPhone1',
+       'ContactPhone2', 'CompanyPhone2', 'ContactPhone3', 'CompanyPhone3',
+       'LinkedInContactURL', 'LinkedInCompanyURL', 'AdvertisingIntelligence',
+       'AlexaScore', 'CompanyNews', 'EmployeeReviews', 'GoogleFinance',
+       'GoogleResearch', 'JobPostings', 'LocalSportsTeams', 'LocalWeather',
+       'News', 'PaidSearchIntelligence', 'PaidSearchKeywordsIntelligence',
+       'SearchMarketingIntelligence', 'SecFilings', 'SeoResearch',
+       'SimilarWebsites', 'SocialMediaMentions', 'SocialMediaPosts',
+       'SocialPosts', 'Tweets', 'WebTechnologies', 'WebsiteAudit',
+       'WebsiteAudit2', 'WebsiteGrader', 'Whois', 'Wikipedia', 'YahooFinance'])
+
+with open(file_path+"company_suffix_list.json") as f:  #how to fix path of main file?
+    suffix_list = json.loads(f.read())  
+
+
+class AutoBot: #Optimus Prime here we come!    ##class AutoBot():
+    "autobot"
+
+    def __init__(self,file_path):   # Asking for variable in initializing hampers none file class functionality
+        self.df = pd.read_csv(file_path)
+        self.nameloc = self.df.columns.get_loc("Name")  #Do not define in init, error if file doesnt have Name column
+
+    # Drop if full column/row empty
+    def emptycheck(self):
+        self.df = self.df.dropna(axis= 'columns', how='all')
+        self.df = self.df.dropna(axis= 'index', how='all')
+
+    # Make new columns if they do not exist
+    def makenamecols(self):
+        try:
+            self.df.insert(self.nameloc+1, "FirstName", None)
+            self.df.insert(self.nameloc+2, "MiddleName", None)
+            self.df.insert(self.nameloc+3, "LastName", None)
+        except:
+            return
+
+    # Split original names into 3 columns and assign them to respective columns
+    def splitnames(self):   
+        for i in range(len(self.df["Name"])):
+            try:
+                a, *b, c = self.df.iat[i,self.nameloc].split()
+                s = " "
+                b = s.join(b)
+            except:
+                a = self.df.iat[i,self.nameloc]
+                b = None
+                c = None
+            self.df.iat[i,self.nameloc+1] = a
+            self.df.iat[i,self.nameloc+2] = b
+            self.df.iat[i,self.nameloc+3] = c
+            main_df = main_df.append(self.df, ignore_index = True)          #only works as intended if both csv files have the same columns in the same order (I think)
+
+
+    def append_to_suffix(self, new_suffix):
+        suffix_set = set(suffix_list)
+        suffix_set.add(new_suffix)
+        suffix_list = list(suffix_set)
+        out_file = open(file_path+"company_suffix_list.json", "w")      
+        json.dump(suffix_list, out_file, indent = 6)      
+        out_file.close()
+
+    # Find and replace company names using a set list
+    def FindReplace(self):
+        for suffix in suffix_list:
+            c = self.df.columns.get_loc("Company Name")  #need to create dialog box to ask person what the name of the column with company names is
+            for i in range(len(self.df)):
+                self.df.iat[i,c] = self.df.iat[i,c].rstrip("\ \,?\ ?{}\.?|\,\ ?{}\.?".format(suffix,suffix))
+
+    # Run all functions above, edit original file()
+    # Dont link functions inside a class, do this outside and call individual functions to execute task : Shetty
+    def fullsplit(self):
+        self.emptycheck()
+        self.makenamecols()
+        self.splitnames()
+        self.FindReplace()
+        self.df.drop_duplicates()
+
+    def save_to_csv(self):
+       main_df.to_csv(output_file_path+'.csv', index=False) #tkinter will allow to pick filepath and file name
+       main_df.to_csv(file_path+'.csv', index=False) #tkinter will allow to pick filepath and file name
+
+
